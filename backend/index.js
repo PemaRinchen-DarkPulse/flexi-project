69c3a57c3cdff42878127eaf721eeb6833784cbd
Pema Rinchen <pemarinchen.12.31.2002@gmail.com>
2024-12-16 18:20:44 +0530

    ADDed cpnfig

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express=require('express')
-
 const app=express()
+const dotenv=require('dotenv')
+const path=require('path');
+dotenv.config({path:path.resolve(__dirname,'./config/.env')});
+const port=process.env.PORT;
+const mongo_url=process.env.MONGO_URL;
 
 app.get("/",(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello from the port")
 })
 
-app.listen(3000,()=>{
-    console.log("Hello");
+app.listen(port,()=>{
+    console.log(port);
 })
